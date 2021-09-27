976b9ed318c6ee6d52168ff870fd9a2abda63fbf
Filip Berndtsson <berndtssonfilip@gmail.com>
2021-09-27 14:05:08 +0200

    Added the code written by Jas!

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,18 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
+library(httr)
+library(dplyr)
+
+api_url <- paste("https://data.riksdagen.se/voteringlista/?rm=2020%2F21&bet=&punkt=&valkrets=&rost=&iid=&sz=10000&utformat=json&gruppering=namn")
+
+raw_api_result <- httr::GET(api_url)
+
+httr::headers(raw_api_result)
+
+str(content(raw_api_result))
+
+
+result_list <- content(raw_api_result)
+str(result_list)
+result_df <- dplyr::bind_rows(result_list$votering$votering)
